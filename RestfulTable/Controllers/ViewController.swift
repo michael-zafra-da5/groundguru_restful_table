8f0e84ea9730b5e36d8a94758bc346bbeb7bb789
zafyraminx <zafra_angelo_26@yahoo.com.ph>
2022-01-19 11:57:03 +0800

    - Added CollectionView - Added condition for list and grid

--- RestfulTable/Controllers/ViewController.swift
+++ RestfulTable/Controllers/ViewController.swift
@@ -12,15 +12,40 @@ import AlamofireImage
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var listDisplaybtn: UIButton!
     
     var cellReuseIdentifier = "userCell"
     var userList: [UserData] = []
+    
+    //Collection
+    let inset: CGFloat = 30
+    let cellsPerRow = 2
+    let minimumLineSpacing: CGFloat = 10
+    let minimumInteritemSpacing: CGFloat = 10
+    
+    
+    @IBAction func taplistDisplay(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        self.updateDisplay(isList: sender.isSelected)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableview.delegate = self
         tableview.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView?.contentInsetAdjustmentBehavior = .always
+        
+        listDisplaybtn.isSelected = false
+        
+        listDisplaybtn.setImage(UIImage(named: "ic_grid")?.resizeImage(48, opaque: false), for: .selected)
+        listDisplaybtn.setImage(UIImage(named: "ic_list")?.resizeImage(48, opaque: false), for: .normal)
+//        listDisplaybtn.setImage(UIImage(), for: .disabled)
+//        listDisplaybtn.setTitle("", for: .normal)
+//        listDisplaybtn.setTitle("no check", for: .selected)
         
         //Start loading...
         AF.request("https://reqres.in/api/users?page=1").responseString(completionHandler: { response in
@@ -35,7 +60,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     if let responseListData = try? decoder.decode(ListResponse.self, from: data) {
                         print("responseListData \(responseListData)")
                         self.userList = responseListData.data
-                        self.tableview.reloadData()
+                        //                        self.tableview.reloadData()
+                        self.updateDisplay(isList: false)
                     }
                 } catch {
                     // handle error
@@ -86,5 +112,84 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func updateDisplay(isList:Bool){
+        if isList {
+            tableview.isHidden = false
+            collectionView.isHidden = true
+            tableview.reloadData()
+        } else {
+            tableview.isHidden = true
+            collectionView.isHidden = false
+            collectionView.reloadData()
+        }
+    }
 }
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.userList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "userCollectionCell", for: indexPath as IndexPath) as! UserCollectionCell
+        
+        let user = userList[indexPath.row]
+        cell.nameLbl.text = "\(user.first_name) \(user.last_name)"
+        cell.email.text = user.email
+        
+        //Alamofire Image
+        AF.request(user.avatar, method: .get).response { response in
+            guard let image = UIImage(data:response.data!) else {
+                // Handle error
+                return
+            }
+            let imageData = image.jpegData(compressionQuality: 1.0)
+            cell.avatarImageView.image = UIImage(data : imageData!)
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Details", bundle: nil)
+        let detailVC = storyboard.instantiateViewController(withIdentifier: "detailSB") as! DetailsViewController
+        detailVC.segueData = userList[indexPath.row]
+        self.present(detailVC, animated: true, completion: nil)
+    }
+    
+    //For Cell Layout
+    //    func collectionView(_ collectionView: UICollectionView,
+    //                        layout collectionViewLayout: UICollectionViewLayout,
+    //                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+    //        return CGSize(width: 90, height: 180)
+    //    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return minimumInteritemSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout
+                        collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return minimumLineSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let marginsAndInsets = inset * 2 + collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right + minimumInteritemSpacing * CGFloat(cellsPerRow - 1)
+        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView?.collectionViewLayout.invalidateLayout()
+        super.viewWillTransition(to: size, with: coordinator)
+    }
+}
+
 
---
+++ RestfulTable/Views/UserCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserCollectionCell.swift
+//  RestfulTable
+//
+//  Created by Michael Angelo Zafra on 1/19/22.
+//
+
+import Foundation
+import UIKit
+
+class UserCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var email: UILabel!
+}
